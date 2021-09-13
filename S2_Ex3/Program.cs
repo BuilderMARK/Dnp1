4fc358ec918a83170973b32ff6adc24769bc470f
BuilderMARK <73698747+BuilderMARK@users.noreply.github.com>
2021-09-13 18:58:31 +0200

    Manglet at commit mainen

---
+++ S2_Ex3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace S2_Ex3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Starting......"); 
+            Car car = new Car();
+            var Getcars = car.GetCars();
+
+           var redCars =  Getcars.FindAll(car => car.Color.Equals("Red"));
+           for (int i = 0; i < redCars.Count; i++)
+           {
+
+               Console.WriteLine(redCars[i]);
+
+            
+           }
+
+           Console.WriteLine("Next sheach");
+
+           var blueCarsWithManuel =
+               Getcars.FindAll(car1 => car1.IsManualShift.Equals(true) && car1.Color.Equals("Blue"));
+           for (int i = 0; i < blueCarsWithManuel.Count; i++)
+           {
+               
+               Console.WriteLine(blueCarsWithManuel[i]);
+           }
+
+
+        }
+
+        
+    }
+}
